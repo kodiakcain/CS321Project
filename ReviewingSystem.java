77a218b0ce5ff22051bd19110b7299e37c604123
kodiakcain <77345292+kodiakcain@users.noreply.github.com>
2023-10-10 14:13:04 -0400

    Shell code for ReviewingSystem - CC

---
+++ ReviewingSystem.java
@@ -0,0 +1,43 @@
+public class ReviewingSystem {
+    public boolean reviewStatus;
+
+    public void loadReviewerScreen() {
+
+    }
+
+    public int getNextReviewID() {
+        int ID = 0;
+
+        return ID;
+    }
+
+    public InfectedUserData getNextReviewObject() {
+        InfectedUserData revObj = new InfectedUserData(null, null, null);
+
+        return revObj;
+    }
+
+    public InfectedUserData loadForm() {
+        InfectedUserData nextForm = new InfectedUserData(null, null, null);
+
+        return nextForm;
+    }
+
+    public boolean reviewForm() {
+        boolean reviewed = true;
+
+        return reviewed;
+    }
+
+    public void editForm() {
+
+    }
+
+    public void editFormState() {
+
+    }
+
+    public void submitForm() {
+
+    }
+}
