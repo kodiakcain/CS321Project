4363ab1c07d9803140d10b01f6d604df995f6f3c
kodiakcain <77345292+kodiakcain@users.noreply.github.com>
2023-10-11 13:13:58 -0400

    Changes to tests and added some javadocs

--- DiseaseManagementSystem/InfectedUserData.java
+++ DiseaseManagementSystem/InfectedUserData.java
@@ -1,4 +1,5 @@
 package DiseaseManagementSystem;
+
 public class InfectedUserData {
     String date = "";
     int formNumber = 0;
@@ -8,60 +9,183 @@ public class InfectedUserData {
     String birthdate = "";
     int numChildren = 0;
 
-    //0 is false, 1 is true
+    // 0 is false, 1 is true
     boolean isGuardian = false;
 
-    //1 is true
+    // 1 is true
     boolean isParent = false;
 
-    public InfectedUserData (String data, String email, String birthdate, int numChildren, boolean isGuardian, boolean isParent) {
+    public InfectedUserData(String data, String email, String birthdate, int numChildren, boolean isGuardian,
+            boolean isParent) {
         this.data = data;
         this.email = email;
         this.birthdate = birthdate;
         this.numChildren = numChildren;
-        this.isGuardian =  isGuardian;
+        this.isGuardian = isGuardian;
         this.isParent = isParent;
     }
 
+    
+    /** 
+     * Get the data.
+     * @return Returns the data. 
+     */
     public String getData() {
-        return this.data; 
+        return this.data;
     }
 
+    
+    /** 
+     * Get the length of the data.
+     * @return Returns the length of the data.
+     */
     public int getDataSize() {
-        return this.data.length(); 
+        return this.data.length();
     }
 
+    
+    /** 
+     * Get the email.
+     * @return Returns the email.
+     */
+    public String getEmail() {
+        return this.email;
+    }
+
+    /**
+     * Get the length of the Email.
+     * @return Returns the length of the email.
+     */
+    public int getEmailLength() {
+        return this.email.length();
+    }
+
+    /**
+     * Get the birthdate.
+     * @return Returns the birthdate.
+     */
+    public String getBirthdate() {
+        return this.birthdate;
+    }
+
+    /**
+     * Get length of birthdate.
+     * @return Returns length of the birthdate.
+     */
+    public int getBirthdateLength() {
+        return this.birthdate.length();
+    }
+
+    /**
+     * Get number of children.
+     * @return Returns number of children.
+     */
+    public int getNumChildren() {
+        return this.numChildren;
+    }
+
+    /**
+     * Setter for email.
+     * @param email New email.
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Setter for form number.
+     * @param number Form number.
+     */
+    public void setFormNumber(int number) {
+        this.formNumber = number;
+    }
+
+    /**
+     * Setter for the data.
+     * @param data New data.
+     */
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    /**
+     * Setter for the date.
+     * @param date New date.
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    /**
+     * Setter for the birthdate.
+     * @param birthdate New birthdate.
+     */
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    /**
+     * Setter for number of children.
+     * @param numchildren New number of children.
+     */
+    public void setNumChildren(int numchildren) {
+        this.numChildren = numchildren;
+    }
+
+    /**
+     * Validates length of Infectious data.
+     * @param data Infectious data.
+     * @return Returns 0 for invalid, 1 for valid. 
+     */
     public int validateInfectiousData(String data) {
         int flag = 0;
 
         return flag;
     }
-    public void storeAllData() {
 
-    }
-    public void addEntry() {
-
-    }
     public int validateEmail(String email) {
-        int flag = 0;
 
-        return flag;
+        if (email.length() < 5) {
+            return 0;
+        } else if (email.length() > 60) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
+
     public int validateBirthdate(String birthdate) {
-        int flag = 0;
-
-        return flag;
+        if (birthdate.length() < 5) {
+            return 0;
+        } else if (birthdate.length() > 60) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
+
+    public int validateNumChild(int numChild) {
+        if (numChild < 0) {
+            return 0;
+        } else if (numChild > 100) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
     public int calculateFormNumber() {
         int formNumber = 0;
 
         return formNumber;
     }
 
-    public int validateNumChild() {
-        int flag = 0;
+    public void storeAllData() {
 
-        return flag;
+    }
+
+    public void addEntry() {
+
     }
 
 }
--- test/InfectedUserDataTest.java
+++ test/InfectedUserDataTest.java
@@ -24,28 +24,106 @@ public class InfectedUserDataTest {
     public void MoreDataTest() {
         InfectedUserData testobj = new InfectedUserData("431fq432vrv42iu483fni483uv3qrncec93q2qubviv3ircjnr3n2u39nr3iv3v9314c3oiv4vu4nggbrjksadlcmdkoqv3jir3940q31ijfn239i4j130fnrvuinvvnd", null, null, 0, false, false);
 
+        int minLength = 1;
+        int maxLength = 128;
+
+        // Check that the length of the string is within the specified range
+        assertThat(testobj.getDataSize(), both(greaterThanOrEqualTo(minLength)).and(lessThanOrEqualTo(maxLength)));
     }
 
     @Test
-    public void AllNull() {
-        InfectedUserData testobj = new InfectedUserData(null, null, null, 0, false, false);
+    public void EmailLengthTestLess() {
+        InfectedUserData testobj = new InfectedUserData("I have covid.", "cain", "02/15/2003", 0, false, false);
+
+        int minLength = 5;
+        int maxLength = 60;
+
+        // Check that the length of the string is within the specified range
+        assertThat(testobj.getEmailLength(), both(greaterThanOrEqualTo(minLength)).and(lessThanOrEqualTo(maxLength)));
+
+    }
+
+    @Test
+    public void EmailLengthTestMore() {
+        InfectedUserData testobj = new InfectedUserData("I have covid.", "431fq432vrv42iu483fni483uv3qrncec9vkhjtjfcrdhrtgfcgctrhdtrdhk", "02/15/2003", 0, false, false);
+
+        int minLength = 5;
+        int maxLength = 60;
+
+        // Check that the length of the string is within the specified range
+        assertThat(testobj.getEmailLength(), both(greaterThanOrEqualTo(minLength)).and(lessThanOrEqualTo(maxLength)));
+
+    }
+
+    @Test
+    public void BirthdateTestLess() {
+        InfectedUserData testobj = new InfectedUserData("I have a cold.", "youssef@gmail.com", "2/15/2003", 0, false, false);
+
+        int minLength = 10;
+        int maxLength = 10;
+
+        // Check that the length of the string is within the specified range
+        assertThat(testobj.getBirthdateLength(), both(greaterThanOrEqualTo(minLength)).and(lessThanOrEqualTo(maxLength)));
+
+    }
+
+    @Test
+    public void BirthdateTestMore() {
+        InfectedUserData testobj = new InfectedUserData("I have a cold.", "youssef@gmail.com", "02/15/20039", 0, false, false);
+
+        int minLength = 10;
+        int maxLength = 10;
+
+        // Check that the length of the string is within the specified range
+        assertThat(testobj.getBirthdateLength(), both(greaterThanOrEqualTo(minLength)).and(lessThanOrEqualTo(maxLength)));
+
+    }
+
+    @Test
+    public void NegativeChildren() {
+        InfectedUserData testobj = new InfectedUserData("I have a cold.", "youssef@gmail.com", "02/15/2003", -1, false, false);
+
+        int minLength = 0;
+        int maxLength = 100;
+
+        // Check that the length of the string is within the specified range
+        assertThat(testobj.getNumChildren(), both(greaterThanOrEqualTo(minLength)).and(lessThanOrEqualTo(maxLength)));
+
+    }
+
+    @Test
+    public void MoreChildren() {
+        InfectedUserData testobj = new InfectedUserData("I have a cold.", "youssef@gmail.com", "02/15/2003", 101, false, false);
+
+        int minLength = 0;
+        int maxLength = 100;
+
+        // Check that the length of the string is within the specified range
+        assertThat(testobj.getNumChildren(), both(greaterThanOrEqualTo(minLength)).and(lessThanOrEqualTo(maxLength)));
 
     }
 
     @Test
     public void NullData() {
+        InfectedUserData testobj = new InfectedUserData(null, "cain@gmail.com", "02/15/2003", 0, false, false);
+
+
+    }
+
+    @Test
+    public void NullAllString() {
         InfectedUserData testobj = new InfectedUserData(null, null, null, 0, false, false);
 
     }
 
     @Test
     public void NullEmail() {
-        InfectedUserData testobj = new InfectedUserData(null, null, null, 0, false, false);
+        InfectedUserData testobj = new InfectedUserData("I have the flu.", null, "07/13/2001", 0, false, false);
 
     }
 
     public void NullBirthdate() {
-        InfectedUserData testobj = new InfectedUserData(null, null, null, 0, false, false);
+        InfectedUserData testobj = new InfectedUserData("I have human rhinovirus.", "john@gmail.com", null, 0, false, false);
 
     }
 }
