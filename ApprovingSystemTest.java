eb343532c25671379fde8e2c528fcb6ad5530c71
youssef <youssefmelk03@gmail.com>
2023-10-16 13:07:39 -0400

    Updated ApprovingSystemTest and ApprovingSystem

--- ApprovingSystemTest.java
+++ ApprovingSystemTest.java
@@ -6,4 +6,18 @@ import DiseaseManagementSystem.ApprovingSystem;
 
 public class ApprovingSystemTest {
 
+    /**
+     * Checks that the approving system's screen loaded successfully.
+     */
+    @Test
+    public void testloadApproverScreen() {
+        ApprovingSystem approvingSystem = new ApprovingSystem();
+        
+        boolean screen_status = approvingSystem.loadApproverScreen();
+        
+        /* print error message to user if screen failed to load */
+        assertTrue("[TEST FAIL] Approving System screen failed to load",
+        screen_status);
+    }
+
 }
