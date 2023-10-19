c5d4e289edb8d4b213bb8982b222181230f92dd2
youssef <youssefmelk03@gmail.com>
2023-10-18 20:52:22 -0400

    Created WorkflowTableTest

---
+++ WorkflowTableTest.java
@@ -0,0 +1,29 @@
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Tester for the WorkflowTable class
+ */
+public class WorkflowTableTest {
+    @Test
+    public void test1AddReviewID() {
+        
+    }
+    
+    @Test
+    public void test1GetNextReviewID() {
+
+    }
+
+    @Test
+    public void test1AddApproveID() {
+
+    }
+
+    @Test
+    public void test1GetNextApproveID() {
+
+    }
+
+}
